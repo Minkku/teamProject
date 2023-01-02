de5a39f763f97504017405d75018c90588b22bae
minkyu_lee <0106995@naver.com>
2023-01-02 14:25:57 +0900

    pots 테이블추가

---
+++ db/migrations/20230102051529_create_pots_table.sql
@@ -0,0 +1,12 @@
+-- migrate:up
+CREATE TABLE pots (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    grey INT NULL,
+    brown INT NULL,
+    brick INT NULL,
+    prod_id int not null, 
+    FOREIGN KEY (prod_id) REFERENCES PRODUCT (id);
+)
+
+-- migrate:down
+DROP TABLE pots;
