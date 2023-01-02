49ff6c1e24e055cdda769d746b93a8417b43bc9a
minkyu_lee <0106995@naver.com>
2023-01-02 14:44:29 +0900

    .

--- db/migrations/20230102051529_create_pots_table.sql
+++ db/migrations/20230102051529_create_pots_table.sql
@@ -1,11 +1,11 @@
 -- migrate:up
 CREATE TABLE pots (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    grey INT NULL,
-    brown INT NULL,
-    brick INT NULL,
+    grey INT NOT NULL,
+    brown INT NOT NULL,
+    brick INT NOT NULL,
     price INT NOT NULL,
-    prod_id int not null, 
+    prod_id INT NOT NULL, 
     FOREIGN KEY (prod_id) REFERENCES PRODUCT (id);
 )
 
